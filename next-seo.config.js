ae7beb37b5f55a59145669fbd2734db3196908c0
conquerorpulkit <impulkit31@gmail.com>
2021-11-27 14:12:00 +0530

    latest commit

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,10 +1,10 @@
 export default {
-  title: "Protfolio | Pulkit Gupta",
+  title: "Portfolio | Pulkit Gupta",
   description: "Real full-stack developer lives here.",
   canonical: "https://protfolio-conquerorpulkit.vercel.app/",
   openGraph: {
     url: "https://protfolio-conquerorpulkit.vercel.app/",
-    title: "Protfolio | Pulkit Gupta",
+    title: "Portfolio | Pulkit Gupta",
     description: "Real full-stack developer lives here.",
     site_name: "PortFolio | Pulkit Gupta",
   },
